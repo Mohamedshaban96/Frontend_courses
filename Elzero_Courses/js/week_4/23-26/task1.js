a8f8b0209732550bd060d8e8a2a72901fe8a642d
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2025-11-06 23:29:39 +0200

    adding tasks 1-7 for week 4 videos 23-26 of js course

---
+++ Elzero_Courses/js/week_4/23-26/task1.js
@@ -0,0 +1,23 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(1e5 ** 1); // 100000
+console.log(100 * 1000); // 100000
+console.log(100000.0000); // 100000
+console.log(100000 / 1); // 100000
+console.log(1000000 - 900000); // 100000
+console.log(100000 % 100001); // 100000
+console.log(Math.round(100000.4)); // 100000
+console.log(Math.ceil(99999.1)); // 100000
+console.log(Math.floor(100000.9)); // 100000
+console.log(Math.trunc(100000.99)); // 100000
+console.log(Math.max(100000, 1 , 2)); // 100000
+console.log(Math.min(100000, 1e6 , 2e6)); // 100000
+console.log(parseInt("100000.45 helloo")); // 100000
+console.log(parseFloat(100000.00)); // 100000
+console.log(+(100000.099999999).toFixed(0)); // 100000
+
